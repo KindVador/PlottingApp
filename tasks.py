fbf4829b961197ebe8d3bd75a8115865502a6417
KindVador <11033778+KindVador@users.noreply.github.com>
2020-10-18 23:26:02 +0200

    Add default tasks for using invoke library

---
+++ tasks.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from invoke import task
+
+
+@task
+def clean(c, docs=False, bytecode=False, extra=''):
+    patterns = ['build']
+    if docs:
+        patterns.append('docs/_build')
+    if bytecode:
+        patterns.append('**/*.pyc')
+    if extra:
+        patterns.append(extra)
+    for pattern in patterns:
+        c.run("rm -rf {}".format(pattern))
+
+
+@task(clean)
+def build(c, docs=False):
+    c.run("python setup.py build")
+    if docs:
+        c.run("sphinx-build docs docs/_build")
+
